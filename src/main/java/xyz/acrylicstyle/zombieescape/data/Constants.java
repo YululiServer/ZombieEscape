8ba1cb2c7af586c6b987077ca054eb21d4d9b362
acrylic-style <me@acrylicstyle.xyz>
2019-08-09 21:11:04 +0900

    overdosed

--- src/main/java/xyz/acrylicstyle/zombieescape/data/Constants.java
+++ src/main/java/xyz/acrylicstyle/zombieescape/data/Constants.java
@@ -11,9 +11,9 @@ public class Constants {
 	public static HashMap<Material, Integer> materialDurability = new HashMap<Material, Integer>();
 
 	static {
-		materialDurability.put(Material.DIRT, 100);
-		materialDurability.put(Material.GRASS, 100);
-		materialDurability.put(Material.WOOD, 300);
+		materialDurability.put(Material.DIRT, 60);
+		materialDurability.put(Material.GRASS, 60);
+		materialDurability.put(Material.WOOD, 200);
 		materialDurability.put(Material.BEDROCK, 50000000);
 		materialDurability.put(Material.COMMAND, 50000000);
 	}
