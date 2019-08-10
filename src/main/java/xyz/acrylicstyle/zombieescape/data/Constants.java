df86be0e61313a9d8f9c9c2a3b3bef84e9fd4cb7
acrylic-style <me@acrylicstyle.xyz>
2019-08-10 23:53:25 +0900

    75% durability

--- src/main/java/xyz/acrylicstyle/zombieescape/data/Constants.java
+++ src/main/java/xyz/acrylicstyle/zombieescape/data/Constants.java
@@ -11,9 +11,9 @@ public class Constants {
 	public static HashMap<Material, Integer> materialDurability = new HashMap<Material, Integer>();
 
 	static {
-		materialDurability.put(Material.DIRT, 60);
-		materialDurability.put(Material.GRASS, 60);
-		materialDurability.put(Material.WOOD, 200);
+		materialDurability.put(Material.DIRT, 45);
+		materialDurability.put(Material.GRASS, 45);
+		materialDurability.put(Material.WOOD, 150);
 		materialDurability.put(Material.COBBLESTONE, 3000);
 		materialDurability.put(Material.BEDROCK, 50000000);
 		materialDurability.put(Material.COMMAND, 50000000);
